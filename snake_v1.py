dddd7e121e9bc26d7da6749b50b0f871d0f36f51
Samuel Bourdeau <samuel.bourdeau@etu.minesparis.psl.eu>
2024-11-06 00:40:49 +0100

    problème de serpent mobile

--- snake_v1.py
+++ snake_v1.py
@@ -9,7 +9,11 @@ import pygame as pg
 pg.init()
 screen = pg.display.set_mode((300, 300))
 clock = pg.time.Clock()
-
+snake = [[10, 15],[11, 15],[12, 15]]    # les coordonnées du corps du serpent
+red=(255,0,0)
+direction = [1, 0]
+white=(255,255,255)
+black=(0,0,0)
 # on rajoute une condition à la boucle: si on la passe à False le programme s'arrête
 running = True
 while running:
@@ -23,37 +27,44 @@ while running:
         # un type de pg.QUIT signifie que l'on a cliqué sur la "croix" de la fenêtre
         if event.type == pg.QUIT:
             running = False
-        # un type de pg.KEYDOWN signifie que l'on a appuyé une touche du clavier
-        elif event.type == pg.KEYDOWN:
-            # si la touche est "Q" on veut quitter le programme
-            if event.key == pg.K_q:
-                running = False
-
+        
     # xxx ici c'est discutable, car si on tape 'q'
     # on va quand même changer de couleur avant de sortir...
    
     #création du damier
     width = 15 
     height = 15
-    color=(255,255,255)
+    
     for i in range(20):
         for j in range(20):
             x=i*15
             y=j*15
             if abs(x-y) % 30==0:
                 rect = pg.Rect(x, y, width, height)
-                pg.draw.rect(screen, color, rect)
-   
-    #création d'un serpent fixe 
-    snake = [[10, 15],[11, 15],[12, 15]]    # les coordonnées du corps du serpent
-    red=(255,0,0)
+                pg.draw.rect(screen, white, rect)
+            else : 
+                rect = pg.Rect(x, y, width, height)
+                pg.draw.rect(screen, black, rect)
+    #serpent qui bouge
+    for event in pg.event.get():
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_q:
+                direction = [-1,0]
+            elif event.key == pg.K_d:
+                direction = [1,0]
+            elif event.key == pg.K_z:
+                direction = [0,1]
+            elif event.key == pg.K_s:
+                direction = [0,-1]
+    first_x, first_y=snake[0]
+    snake.pop()
+    newcell_x, newcell_y=first_x + direction[0], first_y + direction[1]
+    snake.insert(0, [newcell_x, newcell_y])
     pg.draw.rect(screen, red, pg.Rect(15*snake[0][0], 15*snake[0][1], 15, 15))
     pg.draw.rect(screen, red, pg.Rect(15*snake[1][0], 15*snake[1][1], 15, 15))
     pg.draw.rect(screen, red, pg.Rect(15*snake[2][0], 15*snake[2][1], 15, 15))
     
-    
     pg.display.update()
-
 
 # Enfin on rajoute un appel à pg.quit()
 # Cet appel va permettre à Pygame de "bien s'éteindre" et éviter des bugs sous Windows
